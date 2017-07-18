5ac38919bc57a5ae9e0933ad7d35e752fc55e147
Henry AT <yiperu@gmail.com>
2017-07-18 00:32:12 -0400

    🔴 ♻️  Refactor and add more test for assurance new possible bug s detected, that is for remember me always

--- Wallet_TDDTests/BrokerTest.m
+++ Wallet_TDDTests/BrokerTest.m
@@ -11,27 +11,31 @@
 #import "Money.h"
 
 @interface BrokerTest : XCTestCase
-
+@property (nonatomic, strong) Broker * emptyBroker;
+@property (nonatomic, strong) Money *oneDollar;
 @end
 
 @implementation BrokerTest
 
 - (void)setUp {
     [super setUp];
+  self.emptyBroker = [[Broker alloc] init];
+  self.oneDollar = [Money dollarWithAmount:1];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+  self.emptyBroker = nil;
+  self.oneDollar = nil;
     [super tearDown];
 }
 
 - (void)testSimpleReduction {
   
-  Broker *broker = [[Broker alloc] init];
   Money *sum = [[Money dollarWithAmount:5] plus:[Money dollarWithAmount:5]];
   
-  Money *reduced = [broker reduce:sum toCurrency:@"USD"];
+  Money *reduced = [self.emptyBroker reduce:sum toCurrency:@"USD"];
   
   XCTAssertEqualObjects(sum, reduced, @"Conversion to same currency should be a NOP");
 
@@ -40,18 +44,27 @@
 // $10 == e5 2:1
 - (void)testReduction {
 
-  Broker *broker = [[Broker alloc] init];
-  [broker addRate:2 fromCurrency:@"USD" toCurrency:@"EUR"];
+  [self.emptyBroker addRate:2 fromCurrency:@"USD" toCurrency:@"EUR"];
   
   Money *dollar = [Money dollarWithAmount:10];
   Money *euros = [Money euroWithAmount:5];
   
-  Money *converter = [broker reduce:dollar toCurrency:@"EUR"];
+  Money *converter = [self.emptyBroker reduce:dollar toCurrency:@"EUR"];
   
   XCTAssertEqualObjects(converter, euros,@"$10 = e5 2:1");
-  
+
 }
 
+// Con esto no aseguramoe que simepre que intentamos hacer una conversion y no hay tasa de conversion , deberia dar una exepcion.
+- (void)testThatNoRateRaisesException {
+  
+  XCTAssertThrows([self.emptyBroker reduce:self.oneDollar toCurrency:@"EUR"],@"No rate ahould cause exception");
+}
 
+// Una conversion nula no deberia tener ningun efecto
+- (void)testThatNilConversionDoesNotChangeMoney {
+  XCTAssertEqualObjects(self.oneDollar, [self.emptyBroker reduce:self.oneDollar toCurrency:@"USD"],@"A nil conversion should have no effect");
+  
+}
 
 @end
